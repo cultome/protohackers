0636d81f7ac933a5d0939a6f00565083750e14cd
Carlos Soria <csoria@cultome.io>
2023-07-27 23:38:23 -0600

    add smoke test first version

---
+++ lib/protohackers/smoke_test/smoke_test.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+require 'socket'
+
+puts '[*] Waiting for connections...'
+Socket.tcp_server_loop('0.0.0.0', 8080) do |conn|
+  Thread.new do
+    data, * = conn.recvmsg
+    puts "[*] Connection received! [#{data}]"
+    conn.print data
+    conn.close
+  end
+end
